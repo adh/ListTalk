ed1a04a2e4f4b99e3eea734ef8cb7c86f9de0b9d
Aleš Hakl <ales@hakl.net>
2023-04-11 20:01:26 +0200

    prompt and newline ;)

--- tools/repl.c
+++ tools/repl.c
@@ -8,9 +8,11 @@ int main(int argc, char**argv){
     LT_init();
     LT_Reader* reader = LT_Reader_newForStream(stdin);
     for(;;){
+        printf("]=> ");
         LT_Object* expr = LT_Reader_readObject(reader);
         LT_Object* res = LT_eval(expr);
         LT_Object_printOn(res, stdout);
+        printf("\n");
     }
     return 0;
 }
