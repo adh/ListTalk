6745e1e241960b2717b841a4a80a523ad6220b6b
Ales Hakl <ales@hakl.net>
2026-05-14 21:55:58 +0200

    more sane intmax_t to Integer conversion

--- src/classes/Integer.c
+++ src/classes/Integer.c
@@ -9,8 +9,7 @@
 #include <ListTalk/classes/SmallInteger.h>
 #include <ListTalk/macros/decl_macros.h>
 
-#include <inttypes.h>
-#include <stdio.h>
+#include <stddef.h>
 
 struct LT_Integer_s {
     LT_Object base;
@@ -27,24 +26,40 @@ LT_DEFINE_CLASS(LT_Integer) {
 };
 
 LT_Value LT_Integer_from_intmax(intmax_t value){
-    char buffer[64];
+    uintmax_t magnitude;
+    uint32_t limbs[sizeof(uintmax_t) / sizeof(uint32_t)];
+    size_t limb_count = 0;
+    int negative = value < 0;
 
     if (value >= (intmax_t)LT_VALUE_FIXNUM_MIN
         && value <= (intmax_t)LT_VALUE_FIXNUM_MAX){
         return LT_SmallInteger_new((int64_t)value);
     }
 
-    snprintf(buffer, sizeof(buffer), "%" PRIdMAX, value);
-    return LT_BigInteger_new_from_digits(buffer, 10);
+    magnitude = negative
+        ? (uintmax_t)(-(value + 1)) + 1
+        : (uintmax_t)value;
+
+    while (magnitude != 0){
+        limbs[limb_count++] = (uint32_t)magnitude;
+        magnitude >>= 32;
+    }
+
+    return LT_BigInteger_new_from_limbs(negative, limb_count, limbs);
 }
 
 LT_Value LT_Integer_from_uintmax(uintmax_t value){
-    char buffer[64];
+    uint32_t limbs[sizeof(uintmax_t) / sizeof(uint32_t)];
+    size_t limb_count = 0;
 
     if (value <= (uintmax_t)LT_VALUE_FIXNUM_MAX){
         return LT_SmallInteger_new((int64_t)value);
     }
 
-    snprintf(buffer, sizeof(buffer), "%" PRIuMAX, value);
-    return LT_BigInteger_new_from_digits(buffer, 10);
+    while (value != 0){
+        limbs[limb_count++] = (uint32_t)value;
+        value >>= 32;
+    }
+
+    return LT_BigInteger_new_from_limbs(0, limb_count, limbs);
 }
