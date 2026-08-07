4d8bc15b4fbe0a1fc2f11ff92f81df5f8a820565
Ales Hakl <ales@hakl.net>
2026-08-08 00:01:36 +0200

    StringBuilder and ByteVectorBuilder

---
+++ ListTalk/classes/Builder.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2023 - 2026 Ales Hakl
+ */
+
+#ifndef H__ListTalk__Builder__
+#define H__ListTalk__Builder__
+
+#include <ListTalk/macros/decl_macros.h>
+
+LT__BEGIN_DECLS
+
+/* LT_StringBuilder is already the name of the VM's internal byte buffer. */
+LT_DECLARE_CLASS(LT_StringBuilderObject);
+LT_DECLARE_CLASS(LT_ByteVectorBuilder);
+
+LT__END_DECLS
+
+#endif
